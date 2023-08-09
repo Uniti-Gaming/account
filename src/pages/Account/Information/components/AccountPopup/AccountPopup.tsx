803b9cf0832d518defb64cfb2d1c3dcef5278976
bababum <bababum1995@gmail.com>
2023-08-09 18:33:41 +0700

    add popup info

--- src/pages/Account/Information/components/AccountPopup/AccountPopup.tsx
+++ src/pages/Account/Information/components/AccountPopup/AccountPopup.tsx
@@ -1,16 +1,40 @@
 import { FC } from 'react';
 
 import { PopupProps } from '@interfaces/PopupProps';
+import styles from './AccountPopup.module.scss';
 
-import PopupEditInfo from '../PopupEditInfo/PopupEditInfo';
+import LayoutBlock from '@/components/LayoutBlock/LayoutBlock';
+import Popup from '@/components/Popup/Popup';
+import Button from '@/components/Button/Button';
 
-const AccountPopup: FC<PopupProps> = (props) => {
-  const handleSubmit = () => {};
-  
+const AccountPopup: FC<PopupProps> = ({ isOpen, handleClose, title }) => {
+
   return (
-    <PopupEditInfo handleSubmit={handleSubmit} {...props} >
-      <div>AccountPopup</div>
-    </PopupEditInfo>
+    <Popup isOpen={isOpen} handleClose={handleClose}>
+      <LayoutBlock title={title}>
+        <div className={styles.content}>
+          <div className={styles.pair}>
+            <h4 className={styles.label}>Мой ID</h4>
+            <p className={styles.about}>
+              С вашим уникальным ID команда Unite Gaming сможет оперативно помочь
+              вам с решением возникающих проблем.
+            </p>
+          </div>
+          <div className={styles.pair}>
+            <h4 className={styles.label}>Реферальный код</h4>
+            <p className={styles.about}>
+              Приглашайте друзей по реферальному коду -
+              обе стороны получат бонусы!
+            </p>
+          </div>
+          <Button
+            text='Ок'
+            handleClick={handleClose}
+            style={{ margin: '39px auto 0', display: 'block' }}
+          />
+        </div>
+      </LayoutBlock>
+    </Popup>
   );
 };
 
